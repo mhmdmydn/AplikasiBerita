4363ea743d06e6784c84d63f74dfa5891cff7fb0
MuhammadMayudin <Muhammadmayudin12@gmail.com>
2021-02-10 18:46:59 +0700

    TestCommit

--- DNews/app/src/main/java/com/del/dnews/activity/SplashActivity.java
+++ DNews/app/src/main/java/com/del/dnews/activity/SplashActivity.java
@@ -13,9 +13,13 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import com.del.dnews.App;
 import androidx.appcompat.app.AppCompatDelegate;
+import com.del.dnews.util.DelAppUpdate;
+import androidx.appcompat.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class SplashActivity extends BaseActivity {
 
+    private static final String URL ="https://raw.githubusercontent.com/MuhammadMayudin/DNews/main/update.json";
     private AppIconView img;
     private TextView text;
 
@@ -27,7 +31,7 @@ public class SplashActivity extends BaseActivity {
         initViews();
         initLogic();
         initListeners();
-
+        newUpdate();
     }
 
     @Override
@@ -39,23 +43,63 @@ public class SplashActivity extends BaseActivity {
     @Override
     public void initLogic() {
         MainUtils.changeActivityFont(this, "sans_regular");
-
-        new Handler().postDelayed(new Runnable(){
-
-                @Override
-                public void run() {
-                    Intent next = new Intent();
-                    next.setClass(getApplicationContext(), MainActivity.class);
-                    startActivity(next);
-                    finish();
-                }
-            }, 3000);
-
+        
     }
 
     @Override
     public void initListeners() {
-
+        
     }
+    
+    private void newUpdate(){
+        new DelAppUpdate(SplashActivity.this, new DelAppUpdate.UpdateListener(){
 
+                @Override
+                public void onResponse(int newVersion, String changeLog,final String UrlDownload) {
+                    if (DelAppUpdate.getCurrentVersionCode(SplashActivity.this) < newVersion) {
+                        new AlertDialog.Builder(SplashActivity.this)
+                            .setTitle("Update available")
+                            .setMessage(changeLog)
+                            .setCancelable(false)
+                            .setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    DelAppUpdate.startDownloadApp(SplashActivity.this, UrlDownload);
+                                }
+                            })
+                            .show();
+                    } else{
+                        //Action no update
+                        new Handler().postDelayed(new Runnable(){
+
+                                @Override
+                                public void run() {
+                                    Intent next = new Intent();
+                                    next.setClass(getApplicationContext(), MainActivity.class);
+                                    startActivity(next);
+                                    finish();
+                                }
+                            }, 3000);
+                        
+                        
+                    }
+                }
+
+                @Override
+                public void onError(String error) {
+                    if(error =="No Connection"){
+                        new Handler().postDelayed(new Runnable(){
+
+                                @Override
+                                public void run() {
+                                    Intent next = new Intent();
+                                    next.setClass(getApplicationContext(), MainActivity.class);
+                                    startActivity(next);
+                                    finish();
+                                }
+                            }, 3000);
+                    }
+                }
+            }).execute(URL);
+    }
 }
